33a5843bb604f4620f2c1f8fdadb63944aaed1f4
juozas <muleviciusj@gmail.com>
2026-05-20 01:35:37 +0300

    element alignment changes on navbar

--- frontend/src/pages/Components/NavBar.jsx
+++ frontend/src/pages/Components/NavBar.jsx
@@ -1,9 +1,7 @@
 import { useAuth } from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
 
 function NavBar() {
   const { user, logout, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
 
   return (
     <div className="navbar bg-base-200">
@@ -11,42 +9,29 @@ function NavBar() {
         <a className="text-lg font-bold">MySavings</a>
       </div>
       <div className="flex grow justify-end px-2">
-        <div className="flex items-stretch">
+        <div className="flex items-center justify-center">
           {user ? (
-            <p className="text-lg font-bold content-center">
-              Sveiki, {user.username}!
-            </p>
+            <p className="text-lg font-bold ">Sveiki, {user.username}!</p>
           ) : (
-            <p className="text-lg font-bold content-center">
-              Neesate prisijungęs
-            </p>
+            <a href="/login" className="btn btn-neutral">
+              Prisijungti
+            </a>
           )}
           <div className="dropdown dropdown-bottom dropdown-end">
-            <div tabIndex={0} role="button" className="btn m-1">
+            <div tabIndex={0} role="button" className="btn">
               ⬇️
             </div>
             <ul
               tabIndex="-1"
               className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
             >
-              {isAuthenticated ? (
+              {isAuthenticated && (
                 <li>
-                  {" "}
                   <button
                     onClick={logout}
                     className="btn btn-neutral mt-4 w-full"
                   >
                     Atsijungti
-                  </button>
-                </li>
-              ) : (
-                <li>
-                  {" "}
-                  <button
-                    onClick={() => navigate("/login")}
-                    className="btn btn-neutral mt-4 w-full"
-                  >
-                    Prisijungti
                   </button>
                 </li>
               )}
