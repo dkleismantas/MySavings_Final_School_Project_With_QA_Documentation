e7362215b760f8493cea93892a26e66c16fc80fd
juozas <muleviciusj@gmail.com>
2026-05-19 12:54:24 +0300

    added login page to React

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,9 +1,67 @@
+import { useState } from "react";
+
 function SignUpForm() {
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    <form>
-        // prisijungimo forma cia (react hook form, daisyui)
-    </form>
-  )
+    <div className="h-screen flex">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 m-auto">
+        <div className="tabs tabs-box">
+          <input
+            type="radio"
+            name="my_tabs_1"
+            className="tab flex-1 "
+            aria-label="Prisijungti"
+            checked={isLogin}
+            onChange={() => setIsLogin(true)}
+          />
+          <input
+            type="radio"
+            name="my_tabs_1"
+            className="tab flex-1"
+            aria-label="Registruotis"
+            checked={!isLogin}
+            onChange={() => setIsLogin(false)}
+          />
+        </div>
+        {isLogin ? (
+          <>
+            <form action="">
+              <label className="label pt-5">El. paštas</label>
+              <input type="email" className="input" placeholder="El. paštas" />
+
+              <label className="label pt-5">Slaptažodis</label>
+              <input type="password" className="input" placeholder="Slaptažodis" />
+            </form>
+            <button className="btn btn-neutral mt-4">Prisijungti</button>
+            <p className="text-center pt-5">
+              Neesate prisiregistravę?{" "}
+              <a href="#"  onClick={(e) => {
+                e.preventDefault();
+                setIsLogin(false);
+              }} className="link link-primary">
+                Registruokitės
+              </a>
+            </p>
+          </>
+        ) : (
+          <>
+            <form action="">
+              <label className="label pt-5">Vartotojo vardas</label>
+              <input type="text" className="input" placeholder="Vartotojo vardas" />
+
+              <label className="label pt-5">El. paštas</label>
+              <input type="email" className="input" placeholder="El. paštas" />
+
+              <label className="label pt-5">Slaptažodis</label>
+              <input type="password" className="input" placeholder="Įveskite slaptažodį" />
+              <input type="password" className="input" placeholder="Pakartokite slaptažodį" />
+            </form>
+            <button className="btn btn-neutral mt-4">Registruotis</button>
+          </>
+        )}
+      </fieldset>
+    </div>
+  );
 }
 
-export default SignUpForm
+export default SignUpForm;
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import SignUpForm from "../components/SignUpForm";
+
+function LoginPage() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  )
+}
+
+export default LoginPage
