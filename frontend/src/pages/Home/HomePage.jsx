edca70bf7926dda4fd12cf4715e09ca13af34355
Karolina Maščinskaitė <kmkarole@yahoo.com>
2026-05-26 12:19:58 +0300

    Add monthly chart

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { getDeposits } from "../../services/Deposits";
+import { getDeposits, getMonthlyDepositsSummary } from "../../services/Deposits";
 import { getSavingGoalsByUserId } from "../../services/Goal"; 
+
 import NavBar from "../Components/NavBar";
 import SummaryMain from "../Components/SummaryMain";
+import MonthlyChart from "../Components/MonthlyChart";
 
 function HomePage() {
   const { user } = useContext(AuthContext);
@@ -11,6 +13,7 @@ function HomePage() {
   const [deposits, setDeposits] = useState([]);
   const [goals, setGoals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [monthlyData, setMonthlyData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,6 +29,23 @@ function HomePage() {
         setGoals(goalsRes);
       } catch (error) {
         console.error("Failed to fetch data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getMonthlyDepositsSummary();
+
+        setMonthlyData(res.data);
+      } catch (err) {
+        console.error(err);
       } finally {
         setLoading(false);
       }
@@ -56,6 +76,7 @@ function HomePage() {
           activeCount={activeGoalsCount}
           completedCount={completedGoalsCount}
         />
+        <MonthlyChart data={monthlyData} />
       </main>
     </>
   );
